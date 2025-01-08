00a2a1736604348e89ccd2bb099ae27b86196e79
OmarAhmedHabib <omarahmedhabib6@gmail.com>
2025-01-08 10:47:27 +0200

    new-updet

--- README.md
+++
@@ -1,2 +0,0 @@
-# Calculator-microsoft
-Calculator microsoft
---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = '';
+}
+
+function deleteLast() {
+    let currentDisplay = document.getElementById("display").value;
+    document.getElementById("display").value = currentDisplay.slice(0, -1);
+}
+
+function calculate() {
+    let expression = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(expression);
+    } catch (e) {
+        document.getElementById("display").value = "Error";
+    }
+}
+function myFunction() {
+    var element = document.getElementById("contenir");
+    element.classList.toggle("dark-mode");
+ }
